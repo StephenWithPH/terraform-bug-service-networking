121327a68696e7d06ffd384870dea7f3fecb843e
stephenwithph <stephenwithph@users.noreply.github.com>
2019-01-09 17:36:48 -0800

    switch to aliased provider

--- managed-services.tf
+++ managed-services.tf
@@ -1,6 +1,7 @@
 resource "google_service_networking_connection" "private_vpc_connection" {
-  #provider = "google-beta.workaround" # works
-  provider = "google-beta" # does not work
+  provider = "google-beta.workaround" # works
+
+  #provider = "google-beta" # does not work
 
   network                 = "${google_compute_network.example.name}"
   service                 = "${var.gcp_managed_services_name}"
